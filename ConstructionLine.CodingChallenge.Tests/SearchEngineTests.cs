550bc6e20ae5676f0e28e12c22319d79c4d2e3ec
Marcin Bebenek <bebenekmarcin@gmail.com>
2019-09-24 22:19:05 +0100

    Implement SearchEngine to filter shirts by color and size

--- ConstructionLine.CodingChallenge.Tests/SearchEngineTests.cs
+++ ConstructionLine.CodingChallenge.Tests/SearchEngineTests.cs
@@ -8,7 +8,46 @@ namespace ConstructionLine.CodingChallenge.Tests
     public class SearchEngineTests : SearchEngineTestsBase
     {
         [Test]
-        public void Test()
+        public void WhenSearchOptionsNotProvided_ShouldThrowException()
+        {
+            var shirts = new List<Shirt>();
+            var searchEngine = new SearchEngine(shirts);
+            SearchOptions searchOptions = null;
+
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                searchEngine.Search(searchOptions);
+            });
+        }
+
+        [Test]
+        public void WhenColorsInSearchOptionsNotProvided_ShouldThrowException()
+        {
+            var shirts = new List<Shirt>();
+            var searchEngine = new SearchEngine(shirts);
+            SearchOptions searchOptions = new SearchOptions { Colors = null };
+
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                searchEngine.Search(searchOptions);
+            });
+        }
+
+        [Test]
+        public void WhenSizesInSearchOptionsNotProvided_ShouldThrowException()
+        {
+            var shirts = new List<Shirt>();
+            var searchEngine = new SearchEngine(shirts);
+            SearchOptions searchOptions = new SearchOptions { Colors = new List<Color>(), Sizes = null };
+
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                searchEngine.Search(searchOptions);
+            });
+        }
+
+        [Test]
+        public void WhenSearchingForRedAndAnySizeShirts_ShouldReturnOnlyRedShirt()
         {
             var shirts = new List<Shirt>
             {
@@ -16,12 +55,126 @@ namespace ConstructionLine.CodingChallenge.Tests
                 new Shirt(Guid.NewGuid(), "Black - Medium", Size.Medium, Color.Black),
                 new Shirt(Guid.NewGuid(), "Blue - Large", Size.Large, Color.Blue),
             };
-
             var searchEngine = new SearchEngine(shirts);
-
             var searchOptions = new SearchOptions
             {
-                Colors = new List<Color> {Color.Red}
+                Colors = new List<Color> { Color.Red }
+            };
+
+            var results = searchEngine.Search(searchOptions);
+
+            AssertResults(results.Shirts, searchOptions);
+            AssertSizeCounts(shirts, searchOptions, results.SizeCounts);
+            AssertColorCounts(shirts, searchOptions, results.ColorCounts);
+        }
+
+        [Test]
+        public void WhenSearchingForRedAndSmallSizeShirts_ShouldReturnOnlyRedSmallShirt()
+        {
+            var shirts = new List<Shirt>
+            {
+                new Shirt(Guid.NewGuid(), "Red - Small", Size.Small, Color.Red),
+                new Shirt(Guid.NewGuid(), "Red - Small", Size.Small, Color.Red),
+                new Shirt(Guid.NewGuid(), "Red - Medium", Size.Medium, Color.Red),
+                new Shirt(Guid.NewGuid(), "Black - Medium", Size.Medium, Color.Black),
+                new Shirt(Guid.NewGuid(), "Blue - Large", Size.Large, Color.Blue),
+            };
+            var searchEngine = new SearchEngine(shirts);
+            var searchOptions = new SearchOptions
+            {
+                Colors = new List<Color> { Color.Red },
+                Sizes = new List<Size> { Size.Small }
+            };
+
+            var results = searchEngine.Search(searchOptions);
+
+            AssertResults(results.Shirts, searchOptions);
+            AssertSizeCounts(shirts, searchOptions, results.SizeCounts);
+            AssertColorCounts(shirts, searchOptions, results.ColorCounts);
+        }
+
+        [Test]
+        public void WhenSearchingForMultipleColorsAndMultipleSizes_ShouldReturnCorrectResults()
+        {
+            var shirts = new List<Shirt>
+            {
+                new Shirt(Guid.NewGuid(), "Red - Small", Size.Small, Color.Red),
+                new Shirt(Guid.NewGuid(), "Red - Small", Size.Small, Color.Red),
+                new Shirt(Guid.NewGuid(), "Red - Medium", Size.Medium, Color.Red),
+                new Shirt(Guid.NewGuid(), "Black - Medium", Size.Medium, Color.Black),
+                new Shirt(Guid.NewGuid(), "Blue - Large", Size.Large, Color.Blue),
+            };
+            var searchEngine = new SearchEngine(shirts);
+            var searchOptions = new SearchOptions
+            {
+                Colors = new List<Color> { Color.Red, Color.Black },
+                Sizes = new List<Size> { Size.Small, Size.Medium }
+            };
+
+            var results = searchEngine.Search(searchOptions);
+
+            AssertResults(results.Shirts, searchOptions);
+            AssertSizeCounts(shirts, searchOptions, results.SizeCounts);
+            AssertColorCounts(shirts, searchOptions, results.ColorCounts);
+        }
+
+
+        [Test]
+        public void WhenSearchingForAnyColorAndAnySizeShirts_ShouldReturnInputShirtResult()
+        {
+            var shirts = new List<Shirt>
+            {
+                new Shirt(Guid.NewGuid(), "Red - Small", Size.Small, Color.Red),
+                new Shirt(Guid.NewGuid(), "Black - Medium", Size.Medium, Color.Black),
+                new Shirt(Guid.NewGuid(), "Blue - Large", Size.Large, Color.Blue),
+            };
+            var searchEngine = new SearchEngine(shirts);
+            var searchOptions = new SearchOptions
+            {
+            };
+
+            var results = searchEngine.Search(searchOptions);
+
+            AssertResults(results.Shirts, searchOptions);
+            AssertSizeCounts(shirts, searchOptions, results.SizeCounts);
+            AssertColorCounts(shirts, searchOptions, results.ColorCounts);
+        }
+
+        [Test]
+        public void WhenSearchingForNonMatchingColor_ShouldReturnNoResults()
+        {
+            var shirts = new List<Shirt>
+            {
+                new Shirt(Guid.NewGuid(), "Red - Small", Size.Small, Color.Red),
+                new Shirt(Guid.NewGuid(), "Black - Medium", Size.Medium, Color.Black),
+                new Shirt(Guid.NewGuid(), "Blue - Large", Size.Large, Color.Blue),
+            };
+            var searchEngine = new SearchEngine(shirts);
+            var searchOptions = new SearchOptions
+            {
+                Colors = new List<Color> { Color.Yellow },
+            };
+
+            var results = searchEngine.Search(searchOptions);
+
+            AssertResults(results.Shirts, searchOptions);
+            AssertSizeCounts(shirts, searchOptions, results.SizeCounts);
+            AssertColorCounts(shirts, searchOptions, results.ColorCounts);
+        }
+
+        [Test]
+        public void WhenSearchingForNonMatchingSize_ShouldReturnNoResults()
+        {
+            var shirts = new List<Shirt>
+            {
+                new Shirt(Guid.NewGuid(), "Red - Small", Size.Small, Color.Red),
+                new Shirt(Guid.NewGuid(), "Black - Small", Size.Small, Color.Black),
+                new Shirt(Guid.NewGuid(), "Blue - Large", Size.Large, Color.Blue),
+            };
+            var searchEngine = new SearchEngine(shirts);
+            var searchOptions = new SearchOptions
+            {
+                Sizes = new List<Size> { Size.Medium }
             };
 
             var results = searchEngine.Search(searchOptions);
@@ -32,3 +185,4 @@ namespace ConstructionLine.CodingChallenge.Tests
         }
     }
 }
+
--- ConstructionLine.CodingChallenge/SearchEngine.cs
+++ ConstructionLine.CodingChallenge/SearchEngine.cs
@@ -1,26 +1,39 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ConstructionLine.CodingChallenge
 {
     public class SearchEngine
     {
-        private readonly List<Shirt> _shirts;
+        private readonly IQueryable<Shirt> _shirts;
+
 
         public SearchEngine(List<Shirt> shirts)
         {
-            _shirts = shirts;
-
-            // TODO: data preparation and initialisation of additional data structures to improve performance goes here.
-
+            _shirts = shirts.AsQueryable();
         }
-
 
         public SearchResults Search(SearchOptions options)
         {
-            // TODO: search logic goes here.
+            if (options == null) throw new ArgumentNullException();
+            if (options.Colors == null) throw new ArgumentNullException();
+            if (options.Sizes == null) throw new ArgumentNullException();
+
+            var matchingShirts = _shirts;
+            if (options.Colors.Any())
+                matchingShirts = matchingShirts.Where(s => options.Colors.Contains(s.Color));
+            if (options.Sizes.Any())
+                matchingShirts = matchingShirts.Where(s => options.Sizes.Contains(s.Size));
+
+            var matchingColorCounts = Color.All.Select(c => new ColorCount { Color = c, Count = matchingShirts.Count(s => s.Color == c) });
+            var matchingSizeCounts = Size.All.Select(s => new SizeCount { Size = s, Count = matchingShirts.Count(shirt => shirt.Size == s) });
 
             return new SearchResults
             {
+                Shirts = matchingShirts.ToList(),
+                ColorCounts = matchingColorCounts.ToList(),
+                SizeCounts = matchingSizeCounts.ToList(),
             };
         }
     }
